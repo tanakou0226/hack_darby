fc28ac8a5626629fc0f2a8d76d3cd72e82337e68
tanakou <tanakou.0226@gmail.com>
2021-06-20 08:03:16 +0900

    lang & tech 追加

--- teams_app/admin.py
+++ teams_app/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import Teams
+from .models import Teams,Lang,Tech
+
+@admin.register(Lang)
+class LangAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tech)
+class UserAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Teams)
--- teams_app/models.py
+++ teams_app/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 
+class Lang(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Tech(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Teams(models.Model):
-    name = models.CharField(max_length=32)
-    work = models.CharField(max_length=32)
+    name = models.CharField(max_length=100)
+    work = models.CharField(max_length=100)
     points = models.IntegerField(null=True)
+    langs = models.ManyToManyField(Lang,blank=True)
+    techs = models.ManyToManyField(Tech,blank=True)
 
     def __repr__(self):
         return "{}: {}".format(self.pk, self.name)
