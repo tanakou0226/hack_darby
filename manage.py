9bf376e59bfbd87b5a73c5b9a1bc26adddccaafe
tanakou <tanakou.0226@gmail.com>
2021-06-20 10:44:57 +0900

    azure用

--- manage.py
+++ manage.py
@@ -3,9 +3,10 @@
 import os
 import sys
 
-
-def main():
-    """Run administrative tasks."""
+#ここから自分で 9行目シングルクオーテーション
+"""
+def main(): 
+    """'Run administrative tasks.'"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -20,3 +21,22 @@ def main():
 
 if __name__ == '__main__':
     main()
+"""
+#ここまで
+
+if __name__ == '__main__':
+    # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
+    # on Azure App Service and should use the production settings.
+    settings_module = "myapp.production" if 'WEBSITE_HOSTNAME' in os.environ else 'myapp.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
+    execute_from_command_line(sys.argv)
--- myapp/wsgi.py
+++ myapp/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
+
+#application = get_wsgi_application()
+settings_module = "myaapp.production" if 'WEBSITE_HOSTNAME' in os.environ else 'myapp.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
