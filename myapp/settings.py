0ce2f579f3a68eeffb6952f246ffd643659ae470
tanakou <tanakou.0226@gmail.com>
2021-06-20 12:31:59 +0900

    DEBUG

--- myapp/settings.py
+++ myapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e$f74rokj$^)riok6lvb--5tf7l*1lrvduw%*dz72exjr$m3@@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
